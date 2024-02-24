3b273c0b8d294afeb425a931dc2d7ff765e207fb
kamelot43 <andrepud@mail.ru>
2024-02-24 21:07:24 +0300

    module3-task1 добавил роуты в app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,36 @@
 import Main from './pages/main';
+import Favorites from './pages/favorites';
+import Login from './pages/login';
+import Offer from './pages/offer';
+import PageNotFound from './pages/pageNotFound';
+import {AppRoute} from './const';
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
 
 export default function App() {
-  return <Main cards={Array.from({length:5}, () => '')}/>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<Main cards={Array.from({length:5}, () => '')} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<Favorites />}
+        />
+        <Route
+          path={`${AppRoute.Offer}/:id`}
+          element={<Offer />}
+        />
+        <Route
+          path="*"
+          element={<PageNotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }

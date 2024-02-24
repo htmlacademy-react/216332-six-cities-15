fb56288472c415ce6ee572ecae983b031449e1aa
kamelot43 <andrepud@mail.ru>
2024-02-24 21:24:25 +0300

    module3-task1 добавил класс контейнер

--- src/pages/pageNotFound.tsx
+++ src/pages/pageNotFound.tsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom';
 
 function PageNotFound() {
   return (
-    <div>
+    <div className="container">
       <h1>404. Page not found</h1>
       <Link to="/">Вернуться на главную</Link>
     </div>

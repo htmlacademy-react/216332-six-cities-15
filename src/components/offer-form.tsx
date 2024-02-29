c995aac8fece3088b2de3c0793aaffc37e89578d
kamelot43 <andrepud@mail.ru>
2024-02-29 11:18:18 +0300

    module4-task1 вынес форму в отдельный компонент

---
+++ src/components/offer-form.tsx
@@ -0,0 +1,95 @@
+import {useState, ChangeEvent} from 'react';
+
+export default function OfferForm() {
+  const [formData, setFormData] = useState({
+    rating: '',
+    review: '',
+  });
+
+  const handleFieldChange = (evt: ChangeEvent<HTMLInputElement>): void => {
+    const {name, value} = evt.target;
+    setFormData({...formData, [name]: value});
+  };
+
+  const isChecked = (value: string): boolean => value === formData.rating;
+
+  return (
+    <form className="reviews__form form" action="#" method="post">
+      <label className="reviews__label form__label" htmlFor="review">Your review</label>
+
+      <div className="reviews__rating-form form__rating">
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="5"
+          id="5-stars"
+          type="radio"
+          onChange={handleFieldChange}
+          checked={isChecked('5')}
+        />
+        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
+          <svg className="form__star-image" width="37" height="33">
+            <use xlinkHref="#icon-star"></use>
+          </svg>
+        </label>
+
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="4"
+          id="4-stars"
+          type="radio"
+          onChange={handleFieldChange}
+          checked={isChecked('4')}
+        />
+        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
+          <svg className="form__star-image" width="37" height="33">
+            <use xlinkHref="#icon-star"></use>
+          </svg>
+        </label>
+
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="3"
+          id="3-stars"
+          type="radio"
+          onChange={handleFieldChange}
+          checked={isChecked('3')}
+        />
+        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
+          <svg className="form__star-image" width="37" height="33">
+            <use xlinkHref="#icon-star"></use>
+          </svg>
+        </label>
+
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="2"
+          id="2-stars"
+          type="radio"
+          onChange={handleFieldChange}
+          checked={isChecked('2')}
+        />
+      </div>
+
+      <textarea
+        className="reviews__textarea form__textarea"
+        id="review"
+        name="review"
+        placeholder="Tell how was your stay, what you like and what can be improved"
+        value={formData.review}
+        onChange={handleFieldChange}
+      >
+      </textarea>
+      <div className="reviews__button-wrapper">
+        <p className="reviews__help">
+          To submit review please make sure to set <span className="reviews__star">rating</span> and describe
+          your stay with at least <b className="reviews__text-amount">50 characters</b>.
+        </p>
+        <button className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
+      </div>
+    </form>
+  );
+}

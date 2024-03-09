a5a477c3ce0f56143f197afce3499e56f69055b2
kamelot43 <andrepud@mail.ru>
2024-03-09 10:19:11 +0300

    module5-task1 создал компонент карта

---
+++ src/components/map.tsx
@@ -0,0 +1,60 @@
+import {useRef, useEffect} from 'react';
+import {Icon, Marker, layerGroup} from 'leaflet';
+import {URL_MARKER_CURRENT, URL_MARKER_DEFAULT} from '../const';
+import useMap from '../hooks/use-map';
+import {Offer} from '../types/offer';
+import {City} from '../types/city';
+
+const defaultCustomIcon = new Icon({
+  iconUrl: URL_MARKER_DEFAULT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
+});
+
+const currentCustomIcon = new Icon({
+  iconUrl: URL_MARKER_CURRENT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
+});
+
+type MapProps = {
+  city: City;
+  offers: Offer[];
+  selectedOffer: Offer;
+};
+
+export default function Map(props: MapProps) {
+  const {city, offers, selectedOffer} = props;
+
+  const mapRef = useRef(null);
+  const map = useMap(mapRef, city);
+
+  useEffect(() => {
+    if (map) {
+      const markerLayer = layerGroup().addTo(map);
+      offers.forEach((offer) => {
+        const marker = new Marker({
+          lat: offer.location.latitude,
+          lng: offer.location.longitude
+        });
+
+        marker
+          .setIcon(selectedOffer !== undefined && offer.id === selectedOffer.id
+            ? currentCustomIcon
+            : defaultCustomIcon
+          )
+          .addTo(markerLayer);
+      });
+
+      return () => {
+        map.removeLayer(markerLayer);
+      };
+    }
+  }, [map, offers, selectedOffer]);
+
+  return (
+    <section className="cities__map map" ref={mapRef}>
+
+    </section>
+  );
+}
